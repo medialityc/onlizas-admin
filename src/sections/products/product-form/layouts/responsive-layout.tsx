13a6256a8d31c19e0924123c58fd7e66cf44c407
Ricardo8Abreu <98675302+Ricardo8Abreu@users.noreply.github.com>
2025-08-15 12:04:03 -0400

    style: product form columns are now full width

--- src/sections/products/product-form/layouts/responsive-layout.tsx
+++ src/sections/products/product-form/layouts/responsive-layout.tsx
@@ -5,16 +5,12 @@ import RightColumn from './right-column';
 
 function ResponsiveLayout () {
   return (
-    <div className="grid grid-cols-1 gap-6 lg:gap-8">
+    <div className="flex flex-col gap-6 lg:gap-8">
       {/* Columna Izquierda - 2/3 del ancho en pantallas grandes */}
-      <div className="xl:col-span-2">
         <LeftColumn />
-      </div>
 
       {/* Columna Derecha - 1/3 del ancho en pantallas grandes */}
-      <div className="xl:col-span-1">
         <RightColumn />
-      </div>
     </div>
   );
 }
