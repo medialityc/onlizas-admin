b4537139c272cf4d5d86ac74ba65cb65339d0560
Leandro <leandrosardinas01@gmail.com>
2025-08-15 17:19:12 -0400

    refactor: Remove redundant header title for cleaner layout

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -94,9 +94,6 @@ const Header = () => {
                 </div>
               </Link>
             </div>
-            <h1 className="text-2xl md:text-xl font-bold text-gray-500 dark:text-white-light tracking-tight whitespace-nowrap select-none">
-              Portal de proveedor - ZAZ Express
-            </h1>
           </div>
 
           <div className="flex items-center space-x-1.5 dropdown sm:flex-1  lg:space-x-2 justify-end">
