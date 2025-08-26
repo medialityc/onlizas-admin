1ace9f9c4cd89c1692854bc8a6438e71a20d3af5
Silvio O. <socarrandin@gmail.com>
2025-08-25 20:51:53 -0400

    fix: dark mode

--- src/sections/inventory-provider/components/inventory-provider-form/store-item/store-list.tsx
+++ src/sections/inventory-provider/components/inventory-provider-form/store-item/store-list.tsx
@@ -9,10 +9,10 @@ const StoreList = ({ items }: Props) => {
   if (items?.length === 0) return null;
   return (
     <div>
-      <h3 className="text-lg font-semibold text-gray-900 my-4 flex items-center ">
+      <h3 className="text-lg font-semibold text-gray-900 my-4 flex items-center dark:text-gray-100">
         <IconSettings className="mr-2 w-5 h-5" /> Configuración de tiendas
       </h3>
-      <div className="space-y-6">
+      <div className="gap-2">
         {items?.map((item, index: number) => (
           <StoreItem key={item?.id} title={item.storeName} index={index} />
         ))}
