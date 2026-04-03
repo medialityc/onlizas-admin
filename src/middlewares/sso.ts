51063fa9d27987b532bbede5aa124c62023da43c
AfroAg02 <afro.aguirre1969@gmail.com>
2026-04-03 02:27:02 -0400

    feat: update zas-sso-client to version 1.2.54 and refactor SSO middleware for improved cookie handling

--- src/middlewares/sso.ts
+++ src/middlewares/sso.ts
@@ -1,9 +1,21 @@
 import { middleware } from "@/sso";
 
 export const withSSO: any = (next: any) => async (req: any, event: any) => {
-  const res = await middleware(req);
-  if (res.status !== 200) {
-    return res;
+  const ssoRes = await middleware(req);
+  if (ssoRes.status !== 200) {
+    return ssoRes;
   }
-  return next(req, event);
+
+  for (const cookie of ssoRes.cookies.getAll()) {
+    req.cookies.set(cookie.name, cookie.value);
+  }
+
+  const res = await next(req, event);
+
+  // Copiar las cookies del SSO middleware a la respuesta final
+  for (const cookie of ssoRes.cookies.getAll()) {
+    res.cookies.set(cookie);
+  }
+
+  return res;
 };
